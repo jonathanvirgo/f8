0afccb9f13c929e6fd4019d8740cfeaefc4d7ef0
QD <datnguyenquoc@admicro.vn>
2021-07-01 15:10:37 +0700

    test husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,9 @@ const siteRouter = require('./site');
 
 function route(app) {
 
-    app.use('/news', newsRouter);
+app.use('/news', newsRouter);
 
-    app.use('/', siteRouter);
+app.use('/', siteRouter);
 
 }
 
